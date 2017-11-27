cdc9face9d65cda5d1f5573956d92a4907a8b6ed
Victor QUESNEL <vquesnel@student.42.fr>
2017-11-27 11:50:00 +0100

    submodules

--- src/main.c
+++ src/main.c
@@ -6,7 +6,11 @@
 /*   By: kwiessle <kwiessle@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/23 18:40:23 by kwiessle          #+#    #+#             */
-/*   Updated: 2017/11/27 17:56:05 by vquesnel         ###   ########.fr       */
+/*   Updated: 2017/11/27 17:56:46 by vquesnel         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+>>>>>>> submodules
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,18 +22,29 @@ int  main(int ac, char **av) {
     display_error(1);
     return (0);
   }
-  if (check_argument(av[1]) == _FAILURE) {
-      display_error(2);
-      return (0);
-  }
-  if (check_format(av[1]) == _FAILURE) {
-      display_error(3);
-      return (0);
-  } else {
+
+  // if (check_argument(av[1]) == _FAILURE) {
+
+  //     display_error(2);
+  //     return (0);
+  // }
+  // if (check_format(av[1]) == _FAILURE) {
+  //     display_error(3);
+  //     return (0);
+  // } else {
     double a = get_coeff(super_trim(av[1]), '2');
     double b = get_coeff(super_trim(av[1]), '1');
     double c = get_coeff(super_trim(av[1]), '0');
-    printf("Reduced form: %g * X^2 %c %g * X^1 %c %g * X^0 = 0\n", a, b < 0 ? 45 : 43,  ft_double_abs(b), c < 0 ? 45: 43,  ft_double_abs(c));
+    double d = get_coeff(super_trim(av[1]), '3');
+    if (d != 0 ) {
+      printf("Reduced form: %g * X^3 %c %g * X^2 %c %g * X^1 %c %g * X^0 = 0\n",
+      c, a < 0 ? 45 : 43, ft_double_abs(a), b < 0 ? 45 : 43, ft_double_abs(b), c < 0 ? 45: 43,  ft_double_abs(c));
+      printf("Polynomial degree: %d\n", 3);
+      printf("The polynomial degree is stricly greater than 2, I can't solve.");
+      return (0);
+    }
+    printf("Reduced form: %g * X^2 %c %g * X^1 %c %g * X^0 = 0\n",
+     a, b < 0 ? 45 : 43,  ft_double_abs(b), c < 0 ? 45: 43,  ft_double_abs(c));
     printf("Polynomial degree: %d\n", a == 0 ? b == 0 ? 0 : 1 : 2);
     if ( a == 0 && c == 0 && b == 0) {
       printf("All numbers are solution\n");
